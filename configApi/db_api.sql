fd68d7797ddda1cee65d03b14045f631d69bf486
EduPanage <panagee3@gmail.com>
2024-10-31 20:58:39 -0400

    Completando o BD

--- configApi/db_api.sql
+++ configApi/db_api.sql
@@ -17,3 +17,25 @@ CREATE TABLE Transacoes(
     tipo ENUM ('entrada', 'saida') NOT NULL,
     FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario) ON DELETE CASCADE
 );
+
+CREATE PROCEDURE AdicionarTransacao(
+
+    IN p_id_usuario INT,
+    IN p_valor DECIMAL(10, 2),
+    IN p_tipo ENUM ('entrada', 'saida')
+);
+
+BEGIN
+    INSERT INTO Transacoes (id_usuario, valor, tipo)
+    VALUES (p_id_usuario, p_valor, p_tipo);
+END 
+DELIMITER;
+
+CREATE PROCEDURE RemoverTransacao(
+
+    IN p_id_transacao INT
+)
+BEGIN
+    DELETE FROM Transacoes WHERE id_transacao = p_id_transacao;
+END
+DELIMITER;
